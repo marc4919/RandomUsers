8929ed0061a70f389a93bb705bba56255eb5d576
marc4919 <marcgeek4919@gmail.com>
2025-04-23 11:24:01 +0200

    Initial Commit

---
+++ RandomUsers/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  RandomUsers
+//
+//  Created by Marc Garcia Teodoro on 23/4/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ RandomUsers/RandomUsersApp.swift
@@ -0,0 +1,17 @@
+//
+//  RandomUsersApp.swift
+//  RandomUsers
+//
+//  Created by Marc Garcia Teodoro on 23/4/25.
+//
+
+import SwiftUI
+
+@main
+struct RandomUsersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
